c369da969a0cd06af8877521a5013c21a605c736
William Noboa <96926960+williamnoboa@users.noreply.github.com>
2022-03-11 15:41:28 -0500

    Update initialSetup.js

--- pw-noboa_leon/src/libs/initialSetup.js
+++ pw-noboa_leon/src/libs/initialSetup.js
@@ -4,11 +4,11 @@ export const createRoles = async() => {
 
 
     try {
-        //contar si existen documentos
+        //existen documentos?
         const count = await roles.estimatedDocumentCount()
         if (count > 0) return;
 
-        //ejecutando todas las promesas al mismo tiempo 
+        //ejecutamos las promesas al mismo tiempo 
         const values = await Promise.all([
             new roles({ name: 'usuario' }).save(),
             new roles({ name: 'moderador' }).save(),
